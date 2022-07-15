52eef49918d9979953e2d300dac6a719507d4de5
Suellen Lima <sls24@aluno.ifal.edu.br>
2022-07-14 21:06:59 -0300

    Add files via upload

---
+++ Conversao.py
@@ -0,0 +1,7 @@
+a = float(input('Digite um valor em metros:'))
+
+cm = a*100
+mm = a*1000
+
+print(f'\nO valor em centímetros é {cm}')
+print (f'O valor em milímetros é {mm}')
---
+++ DobroETriplo.py
@@ -0,0 +1,8 @@
+import math 
+
+a = int(input('\nDigite um número:'))
+dobro = a*2
+triplo = a*3
+raizQ = math.sqrt(a)
+
+print(f'O dobro de {a} é {dobro}, o triplo é {triplo} e a raiz quadrada é {raizQ}')
